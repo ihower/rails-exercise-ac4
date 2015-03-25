0e0d99131898ae2184c397c8c50fe6d4517966ea
Wen-Tien Chang <ihower@gmail.com>
2015-03-25 13:59:43 +0800

    Add new column status to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325051517) do
+ActiveRecord::Schema.define(version: 20150325054730) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150325051517) do
     t.integer  "capacity"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "status"
   end
 
   create_table "people", force: :cascade do |t|
