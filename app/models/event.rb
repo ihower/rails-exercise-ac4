b39c4405d0551123308170fb76b9aeee7d724651
Wen-Tien Chang <ihower@gmail.com>
2015-04-08 10:10:56 +0800

    Add dependent => :destroy

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,8 @@ class Event < ActiveRecord::Base
 
   has_many :attendees, :dependent => :destroy
 
-  has_many :event_groupships
+  has_many :event_groupships, :dependent => :destroy
+
   has_many :groups, :through => :event_groupships
 
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
 
-  has_many :event_groupships
+  has_many :event_groupships, :dependent => :destroy
 
   has_many :events, :through => :event_groupships
 
